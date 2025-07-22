4fc8b4c590e63a18d00d7a846dcf2afc401f65f1
Krishna kumar <141641883+krish8986@users.noreply.github.com>
2025-07-22 21:57:53 +0530

    Update ProductDetails.js

--- Aranya-Ecommerce/client/src/pages/ProductDetails.js
+++ Aranya-Ecommerce/client/src/pages/ProductDetails.js
@@ -50,7 +50,7 @@ const ProductDetails = () => {
       <div className="row container product-details">
         <div className="col-md-6">
           <LazyLoadImage
-            src={`/api/v1/product/product-photo/${product._id}`}
+            src={`${process.env.REACT_APP_API}/api/v1/product/product-photo/${product._id}`}
             className="card-img-top"
             alt={product.name}
             height="300"
@@ -84,7 +84,7 @@ const ProductDetails = () => {
           {relatedProducts?.map((p) => (
             <div className="card m-2" key={p._id}>
               <img
-                src={`/api/v1/product/product-photo/${p._id}`}
+                src={`${process.env.REACT_APP_API}/api/v1/product/product-photo/${p._id}`}
                 className="card-img-top"
                 alt={p.name}
                 loading="lazy"
